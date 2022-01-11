7df87b77958550eae582f55f249cd0dc214a673b
Harish20011010 <harrish2137@gmail.com>
2022-01-11 21:03:22 +0530

    added navbar and created page components

---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div>this is home</div>;
+}
+
+export default Home;
---
+++ src/pages/Starred.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Starred() {
+  return <div>this is starred</div>;
+}
+
+export default Starred;
