ea2ad5c4b4c4376b7599eac8da4b45e2516bf946
Harish20011010 <harrish2137@gmail.com>
2022-01-15 12:48:18 +0530

    created two new more hooks: useLastQuerry and useShow

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,73 +1,18 @@
 /* eslint-disable no-underscore-dangle */
-import React, { useEffect, useReducer } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
-import { apiGet } from "../misc/config";
-
 import Details from "../components/show/Details";
 import ShowMainData from "../components/show/ShowMainData";
 import Cast from "../components/show/Cast";
 
 import Seasons from "../components/show/Seasons";
 import { InfoBlock, ShowPageWrapper } from "./Show.styled";
+import { useShow } from "../misc/custom-hooks";
 
 function Show() {
-  const reducer = (prevState, action) => {
-    switch (action.type) {
-      case "FETCH_SUCCESS": {
-        return {
-          isLoading: false,
-          error: null,
-          show: action.show,
-        };
-      }
-
-      case "FETCH_FAILED": {
-        return {
-          ...prevState,
-          isLoading: false,
-          error: action.error,
-        };
-      }
-
-      default:
-        return prevState;
-    }
-  };
-
-  const initialState = {
-    show: null,
-    isLoading: true,
-    error: null,
-  };
-
   const { id } = useParams();
 
-  const [{ show, isLoading, error }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
-
-  // console.log("state", state);
-
-  useEffect(() => {
-    let isMounted = "true";
-
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
-      .then((results) => {
-        if (isMounted) {
-          dispatch({ type: "FETCH_SUCCESS", show: results });
-        }
-      })
-      .catch((err) => {
-        if (isMounted) {
-          dispatch({ type: "FETCH_FAILED", error: err.message });
-        }
-      });
-
-    return () => {
-      isMounted = "false";
-    };
-  }, [id]);
+  const { show, isLoading, error } = useShow(id);
 
   // console.log("show", show);
   // console.log("isLoading", isLoading);
